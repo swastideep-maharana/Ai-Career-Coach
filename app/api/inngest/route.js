744b8907c235b4d2c843d5d292d24ee052387919
swastideep maharana <swastideep67maharana@gmail.com>
2025-05-06 20:27:34 +0530

    #added neon database

---
+++ app/api/inngest/route.js
@@ -0,0 +1,11 @@
+import { inngest } from '@/lib/inngest/client'
+
+import {serve} from 'inngest/next'
+
+
+export const {GET, POST , PUT} = serve({
+    client: inngest,
+    functions:[
+       
+    ],
+})
