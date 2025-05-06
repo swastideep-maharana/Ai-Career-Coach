ddaa7fe4b543bb30c897579eeebeadac7963b08a
swastideep maharana <swastideep67maharana@gmail.com>
2025-05-06 19:27:51 +0530

    #header done

--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
         <body className={` ${inter.className} `}>
           <ThemeProvider
             attribute="class"
-            defaultTheme="light"
+            defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
