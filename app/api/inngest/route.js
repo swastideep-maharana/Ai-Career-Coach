1f664c95c7a7cff866474b958c834b911e52a226
swastideep maharana <swastideep67maharana@gmail.com>
2025-05-10 00:04:13 +0530

    #fixes inngest

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,9 +1,5 @@
 import { serve } from "inngest/next";
-
 import { inngest } from "@/lib/inngest/client";
-import { generateIndustryInsights } from "@/lib/inngest/function";
+import { functions } from "@/lib/inngest/function";
 
-export const { GET, POST, PUT } = serve({
-  client: inngest,
-  functions: [generateIndustryInsights],
-});
+export const { GET, POST } = serve(inngest, functions);
