dbf6af89b1a7216dd8fd98fe3e3b4969899c9f26
swastideep maharana <swastideep67maharana@gmail.com>
2025-05-26 13:07:36 +0530

    #fixes

--- app/(main)/ai-cover-letter/_components/cover-letter-generator.jsx
+++ app/(main)/ai-cover-letter/_components/cover-letter-generator.jsx
@@ -5,13 +5,13 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { toast } from "sonner";
 import { Loader2 } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import { generateCoverLetter } from "@/actions/cover-letter";
+
 import useFetch from "@/hooks/use-fetch";
+import { generateCoverLetter } from "@/actions/cover-letter";
 import { coverLetterSchema } from "@/app/lib/schema";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
